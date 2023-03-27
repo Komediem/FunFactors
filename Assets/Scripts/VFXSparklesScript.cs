526203e2cc7a7a7ff6807cf0086fa17e18c90e0c
Komediem <gbussola@artfx.fr>
2023-03-27 16:47:29 +0200

    1st Git
    [add] Dynamite Asset
    [add] Boat Asset
    [add] Player System
    [add] Sparkles VFX
    
    [refactor] Shoot script
    [refactor] Dynamite script

---
+++ Assets/Scripts/VFXSparklesScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFXSparklesScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform SparklesLocation;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //SparklesLocation.position;
+        //SparklesLocation.rotation;
+    }
+}
